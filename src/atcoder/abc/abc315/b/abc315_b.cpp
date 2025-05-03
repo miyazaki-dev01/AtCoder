4323ac10efa19160e1687bf15cb382e2047aaf6c
miyazaki <miyazaki.dev01@gmail.com>
2025-05-03 20:04:04 +0900

    update : abc315-bの解答

--- src/atcoder/abc/abc315/b/abc315_b.cpp
+++ src/atcoder/abc/abc315/b/abc315_b.cpp
@@ -10,6 +10,24 @@ using vvi = vector<vi>;
 using P = pair<int, int>;
 
 int main() {
-  
+  int m;
+  cin >> m;
+  vi d(m);
+  int sum = 0;
+  rep(i,m) {
+    cin >> d[i];
+    sum += d[i];
+  }
+
+  int mid = (sum + 1) / 2;
+
+  rep(i,m) {
+    if(!((mid - d[i]) <= 0)) {
+      mid -= d[i];
+    } else {
+      cout << i+1 << ' ' << mid << endl;
+      return 0;
+    }
+  }
   return 0;
 }
