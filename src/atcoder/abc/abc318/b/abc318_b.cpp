c805431ffb07e936a5267440774b27014945c6d8
miyazaki <miyazaki.dev01@gmail.com>
2025-05-18 19:03:03 +0900

    update : abc318-bの解答

--- src/atcoder/abc/abc318/b/abc318_b.cpp
+++ src/atcoder/abc/abc318/b/abc318_b.cpp
@@ -10,6 +10,27 @@ using vvi = vector<vi>;
 using P = pair<int, int>;
 
 int main() {
-  
+  int n;
+  cin >> n;
+
+  vector<vector<bool>> s(101, vector<bool>(101, false));
+  rep(i,n) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    for(int i = a; i < b; i++) {
+      for(int j = c; j < d; j++) {
+        s[i][j] = true;
+      }
+    }
+  }
+
+  int ans = 0;
+  rep(i,101) {
+    rep(j,101) {
+      if(s[i][j]) ans++;
+    }
+  }
+
+  cout << ans << endl;
   return 0;
 }
