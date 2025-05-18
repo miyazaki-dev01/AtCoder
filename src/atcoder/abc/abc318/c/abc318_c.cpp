5ef92ea11f1c232993ff07202f0416a88effde9f
miyazaki <miyazaki.dev01@gmail.com>
2025-05-18 20:21:01 +0900

    update : abc318-cの解答

--- src/atcoder/abc/abc318/c/abc318_c.cpp
+++ src/atcoder/abc/abc318/c/abc318_c.cpp
@@ -10,6 +10,33 @@ using vvi = vector<vi>;
 using P = pair<int, int>;
 
 int main() {
-  
+  ll n, d, p;
+  cin >> n >> d >> p;
+  vector<ll> f(n);
+  rep(i,n) cin >> f[i];
+
+  sort(f.rbegin(), f.rend());
+
+  ll ans = 0;
+  for(int i = 0; i < n;) {
+    ll cnt = 0;
+    if(n < i+d) {
+      for(int j = i; j < n; j++) {
+        cnt += f[j];
+      } 
+    } else {
+      for(int j = i; j < i+d; j++) {
+        cnt += f[j];
+      }
+    }
+    if(cnt > p) {
+      ans += p;
+    } else {
+      ans += cnt;
+    }
+    i += d;
+  }
+
+  cout << ans << endl;
   return 0;
 }
